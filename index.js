9e037245d08fad331f01633a7d3cf9181a5a365d
Florian <f.rumigny@gmail.com>
2024-09-11 21:06:11 +0200

    solution

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const app = require("./src/app");
+require("dotenv").config();
 
-const port = 5000;
+const port = process.env.APP_PORT;
 
 app
   .listen(port, () => {
-    console.log(`Server is listening on ${port}`);
+    console.log(`Server is listening on http://localhost:${port}`);
   })
   .on("error", (err) => {
     console.error("Error:", err.message);
