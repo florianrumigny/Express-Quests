55c0461ebaef1fcc3161b17654f2948bf30107c3
Florian <f.rumigny@gmail.com>
2024-09-12 21:24:19 +0200

    get query solution

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,9 +1,28 @@
 const database = require("../../database");
 
 const getUsers = (req, res) => {
+  let initialSql = "select * from users";
+  const sqlValues = [];
+
+  if (req.query.language != null) {
+    initialSql += " where language = ?";
+    sqlValues.push(req.query.language);
+
+    if (req.query.city != null) {
+      initialSql += " and city = ?";
+      sqlValues.push(req.query.city);
+    }
+  } else if (req.query.city != null) {
+    initialSql += " where city = ?";
+    sqlValues.push(req.query.city);
+  }
+
   database
-    .query("select * from users")
+    .query(initialSql, sqlValues)
     .then(([users]) => {
+      if (sqlValues.length < 1) {
+        res.sendStatus(200);
+      }
       res.status(200).json(users);
     })
     .catch((err) => {
